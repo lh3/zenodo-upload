cb762090cf437128f23a5af0185ecca9ab6f3e5d
Heng Li <lh3@me.com>
2020-12-25 14:55:11 -0500

    upload multiple files

--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -7,10 +7,11 @@
 set -xe
 
 DEPOSITION=$1
-FILEPATH=$2
-FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
-
 BUCKET=$(curl -H "Accept: application/json" -H "Authorization: Bearer $ZENODO_TOKEN" "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION" | jq --raw-output .links.bucket)
 
-
-curl --progress-bar -o /dev/null --upload-file $FILEPATH $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
+shift
+for FILEPATH in "$@"
+do
+	FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
+	curl --progress-bar -o /dev/null --upload-file $FILEPATH $BUCKET/$FILENAME?access_token=$ZENODO_TOKEN
+done
